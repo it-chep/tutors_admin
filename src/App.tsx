e6b54e45f449ec1e9aa4dbaa9771f3e01197e577
1ww1s <ctop124@mail.ru>
2025-09-11 23:27:41 +0300

    update. при изменении url страница сверху

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 import { GlobalMessage } from './entities/globalMessage';
 import { GlobalLoading } from './entities/globalLoading';
 import { useAppSelector } from './app/store/store';
+import { useEffect } from 'react';
 
 function App() {
 
   const {isLoading: globalIsLoading} = useAppSelector(s => s.globalLoadingReducer)
   const {globalMessage} = useAppSelector(s => s.globalMessageReducer)
+
+  const {pathname} = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({top: 0})
+  }, [pathname])
 
   return (
     <div className="App">
