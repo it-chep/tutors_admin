4361f1ebedc82e571d28de3ee3663704a6b47ab5
1ww1s <ctop124@mail.ru>
2025-09-15 19:13:32 +0300

    fix. без моковых данных

--- src/entities/subject/api/SubjectService.ts
+++ src/entities/subject/api/SubjectService.ts
@@ -6,27 +6,9 @@ import { ISubject } from "../model/types"
 class SubjectService {
 
     async getAll(){
-        // const res = await fetchAuth(process.env.REACT_APP_SERVER_URL_ADMIN + '/subjects')
-        // const {subjects}: {subjects: ISubject[]} = await res.json()
-
-        return [
-    {
-      "id": 1,
-      "name": "Математика"
-    },
-    {
-      "id": 2,
-      "name": "Русский язык"
-    },
-    {
-      "id": 3,
-      "name": "Физика"
-    },
-    {
-      "id": 4,
-      "name": "Информатика"
-    }
-  ]
+        const res = await fetchAuth(process.env.REACT_APP_SERVER_URL_ADMIN + '/subjects')
+        const {subjects}: {subjects: ISubject[]} = await res.json()
+        return subjects
     }
 
 }
