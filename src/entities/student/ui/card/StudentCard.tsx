9b8490a3f5c9ac1eeaf381ce5835f089f44724dc
Maksim Necheporuk <mnecheporuka@ozon.ru>
2026-01-28 02:18:59 +0300

    Правки

--- src/entities/student/ui/card/StudentCard.tsx
+++ src/entities/student/ui/card/StudentCard.tsx
@@ -11,52 +11,53 @@ interface IProps {
 
 export const StudentCard: FC<IProps & PropsWithChildren> = ({student, children}) => {
 
-    
+
     return (
         <section className={classes.card}>
-            <DataList 
+            <DataList
                 title="Данные студента"
                 list={[
                     `ID: ${student.id}`,
                     `ФИО: ${student.last_name} ${student.first_name} ${student.middle_name}`,
                     `Телефон: ${student.phone}`,
                     `Стоимость часа: ${student.cost_per_hour} ₽`,
-                    <a 
-                        target="_blank" 
-                        href={student.tg} 
+                    <a
+                        target="_blank"
+                        href={student.tg}
                         className={classes.highlight}
                     >
                         Написать в тг
                     </a>,
                     `Предмет: ${student.subject_name}`,
                     <span>Репетитор: <Link to={'/tutor/' + student.tutor_id} className={classes.highlight}>{student.tutor_name}</Link></span>,
-                    Boolean(student.tg_admin_username) 
-                        ? 
+                    Boolean(student.tg_admin_username)
+                        ?
                     `Рабочий аккаунт тг: ${student.tg_admin_username}`
                         :
                     <span className={classes.noTgID}>Нет рабочего аккаунта тг</span>
                 ]}
             />
-            <DataList 
+            <DataList
                 title="Данные родителя"
                 list={[
                     `ФИО: ${student.parent_full_name}`,
                     `Телефон: ${student.parent_phone}`,
-                    <a 
-                        target="_blank" 
-                        href={student.parent_tg} 
+                    <a
+                        target="_blank"
+                        href={student.parent_tg}
                         className={classes.highlight}
                     >
                         Написать в тг
                     </a>,
-                    Boolean(student.tg_id) 
-                        ? 
+                    `Ссылка для прикрепления: https://t.me/Payments_A_bot?start=id_${student.id}`,
+                    Boolean(student.tg_id)
+                        ?
                     `TgID: ${student.tg_id}`
                         :
                     <span className={classes.noTgID}>Нет ТГ ID</span>
                 ]}
             />
-            <DataList 
+            <DataList
                 title="Финансы"
                 list={[
                     `Кошелек: ${student.balance} ₽`,
