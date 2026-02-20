bea4a5af52c7514862582de08f30583af357dbdb
1ww1s <ctop124@mail.ru>
2026-02-20 22:57:43 +0300

    add моки

---
+++ src/app/mocks/assistant/AssistantMocks.ts
@@ -0,0 +1,28 @@
+const urlAdmin = process.env.REACT_APP_SERVER_URL_ADMIN
+
+export const AssistantMocks: {[key: string]: any} = {
+    [`${urlAdmin}/assistant`]: {
+        assistants:  [
+            {
+                id: 1,
+                full_name: 'Калашников Павел Сергеевич',
+                tg: 'TG'
+            },
+            {
+                id: 2,
+                full_name: 'Нечепорук ПС',
+                tg: 'TG FFFFFFF'
+            },
+        ]
+    },
+    [`${urlAdmin}/assistant/:id`]: {
+        assistant: 
+            {
+                id: 1,
+                full_name: 'Калашников ПС',
+                phone: '8 888 888 88 88',
+                tg: 'https://TG...',
+                tg_admins_usernames: ['@kalashnikoff069', '@maxim_jordan']
+            },
+    },
+};
